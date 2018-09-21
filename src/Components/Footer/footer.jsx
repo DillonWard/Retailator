a06017538e1aa568202e80fb37e32892313cb7fd
Dillon Ward <Dillonward13@gmail.com>
2018-09-21 14:19:29 +0200

    changed theme of project

--- src/Components/Footer/footer.jsx
+++ src/Components/Footer/footer.jsx
@@ -7,20 +7,9 @@ class FooterPage extends React.Component {
         return(
             <Footer id='footer' className="font-small pt-4 mt-2">
                 <Container fluid className="text-center text-md-center">
-                    <Row>
-                    <Col sm="3">
-                    <a href="https://www.linkedin.com/in/dillonsward/" target="_blank"><i className="fa fa-2x fa-linkedin"></i></a>
-                    </Col>
-                    <Col sm= "3">
-                    <a href="https://github.com/DillonWard" target="_blank"><i className="fa fa-2x fa-github"/></a>
-                    </Col>
-
-                    <Col sm="6">
                     <Container fluid>
-                        &copy; {(new Date().getFullYear())} Copyright: <a href="https://github.com/DillonWard/Retailator" target="_blank"> Retailator - Dillon Ward </a>
+                        &copy; {(new Date().getFullYear())} Copyright: <a id='cpr' href="https://github.com/DillonWard/Retailator" target="_blank"> Retailator - Dillon Ward </a>
                     </Container>
-                    </Col>
-                    </Row>
                 </Container>
             </Footer>
         );
